39499f577eca229a47418deeaabd3bac04f65095
unknown <amrmostafa5557@gmail.com>
2023-02-06 16:07:24 -0800

    Dynamic programming

--- week4_divide_and_conquer/7_closest_points/closest.py
+++ week4_divide_and_conquer/7_closest_points/closest.py
@@ -2,23 +2,87 @@ from collections import namedtuple
 from itertools import combinations
 from math import sqrt
 
-
 Point = namedtuple('Point', 'x y')
-
-
 def distance_squared(first_point, second_point):
     return (first_point.x - second_point.x) ** 2 + (first_point.y - second_point.y) ** 2
 
-
+#def minimum_distance_squared(points):
 def minimum_distance_squared_naive(points):
     min_distance_squared = float("inf")
-
     for p, q in combinations(points, 2):
         min_distance_squared = min(min_distance_squared,
                                    distance_squared(p, q))
-
     return min_distance_squared
 
+import math
+ 
+ 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+ 
+ 
+def compareX(a, b):
+    p1 = a
+    p2 = b
+    return (p1.x - p2.x)
+ 
+ 
+def compareY(a, b):
+    p1 = a
+    p2 = b
+    return (p1.y - p2.y)
+ 
+ 
+def dist(p1, p2):
+    return math.sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y))
+ 
+ 
+def bruteForce(P, n):
+    min_dist = float("inf")
+    for i in range(n):
+        for j in range(i+1, n):
+            if dist(P[i], P[j]) < min_dist:
+                min_dist = dist(P[i], P[j])
+    return min_dist
+ 
+ 
+def min(x, y):
+    return x if x < y else y
+ 
+ 
+def stripClosest(strip, size, d):
+    min_dist = d
+    strip = sorted(strip, key=lambda point: point.y)
+ 
+    for i in range(size):
+        for j in range(i+1, size):
+            if (strip[j].y - strip[i].y) >= min_dist:
+                break
+            if dist(strip[i], strip[j]) < min_dist:
+                min_dist = dist(strip[i], strip[j])
+    return min_dist
+ 
+ 
+def closestUtil(P, n):
+    if n <= 3:
+        return bruteForce(P, n)
+    mid = n//2
+    midPoint = P[mid]
+    dl = closestUtil(P, mid)
+    dr = closestUtil(P[mid:], n - mid)
+    d = min(dl, dr)
+    strip = []
+    for i in range(n):
+        if abs(P[i].x - midPoint.x) < d:
+            strip.append(P[i])
+    return min(d, stripClosest(strip, len(strip), d))
+ 
+ 
+def closest(P, n):
+    P = sorted(P, key=lambda point: point.x)
+    return closestUtil(P, n)
 
 if __name__ == '__main__':
     input_n = int(input())
@@ -27,5 +91,5 @@ if __name__ == '__main__':
         x, y = map(int, input().split())
         input_point = Point(x, y)
         input_points.append(input_point)
-
+    print("{0:.9f}".format(sqrt(minimum_distance_squared_naive(input_points))))
     print("{0:.9f}".format(sqrt(minimum_distance_squared_naive(input_points))))
--- week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,5 +1,27 @@
 def edit_distance(first_string, second_string):
-    return 0
+    n, m = len(first_string), len(second_string)
+    arr = []
+    subArr = []
+    for i in range(m + 1):
+        subArr.append(i)
+    arr.append(subArr)
+    
+    for i in range(n):
+        subArr = [0] * (m + 1)
+        subArr[0] = i + 1
+        arr.append(subArr)
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+
+            if(first_string[i-1] == second_string[j - 1]):
+                arr[i][j] = arr[i-1][j-1]
+            else:
+                minValue = min(arr[i - 1][j - 1], arr[i][j - 1])
+                minValue = min(minValue, arr[i - 1][j])
+                arr[i][j] = 1 + minValue
+
+    return arr[n][m]
 
 
 if __name__ == "__main__":
