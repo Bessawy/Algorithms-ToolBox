b3a545c3e8def2149ea0e35ed4f95a86001096da
unknown <amrmostafa5557@gmail.com>
2023-02-12 06:24:09 -0800

    knapsack

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,12 +1,33 @@
-from sys import stdin
+def maxWeight(capacity, items):
+    n = len(items)
+    T = [[0] * (capacity + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w in range(1, capacity + 1):
+            T[i][w] = T[i - 1][w]
+            if(w - items[i - 1] >= 0 ):
+                T[i][w] = max(T[i - 1][w - items[i - 1]] + items[i - 1], T[i][w])
+    
+    return T[n][capacity]
 
 
-def maximum_gold(capacity, weights):
-    assert False
+def otimalSolution(T, capacity, items):
+    i = len(items)
+    w = capacity
+    optimal = []
+
+    while(i>0 and w>0):
+        if T[i][w] == T[i-1][w]:
+            i -= i
+        else:
+            optimal.append(items[i - 1])  
+            i-=1
+            w-=items[i-1]  
+    return optimal
 
 
-if __name__ == '__main__':
-    input_capacity, n, *input_weights = list(map(int, stdin.read().split()))
-    assert len(input_weights) == n
-
-    print(maximum_gold(input_capacity, input_weights))
+ipt = input()
+W, n = map(int, ipt.split())
+ipt = input()
+weights = list(map(int, ipt.split()))
+print(maxWeight(W, weights))
